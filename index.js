bf1e2c25d3af113636a37fbb84516e66283305bb
zxm15 <gavingan9e@gmail.com>
2016-01-25 13:04:42 -0500

    uploading a image function finished

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ var handle = {};
 handle["/"] = requestHandler.start;
 handle["/start"] = requestHandler.start;
 handle["/upload"] = requestHandler.upload;
+handle["/show"] = requestHandler.show;
 
 server.start(router.route, handle);
